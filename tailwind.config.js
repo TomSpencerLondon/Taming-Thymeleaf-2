67483e46b7e3d99a7d8b50923fa9b04f8dc4f3ef
Tom Spencer <thomas.spencer@codurance.com>
2022-11-26 10:43:38 +0000

    Add application shell and index.html setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme');
 module.exports = {
   content: ['./src/main/resources/templates/**/*.html'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {sans: ['Inter var', ...defaultTheme.fontFamily.sans],}
+    },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms')
+  ],
 }
